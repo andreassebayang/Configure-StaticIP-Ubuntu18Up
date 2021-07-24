1e740996f1ee20637d9ec2750ddcc6366eeead35
Andreas Sebayang <79214343+andreassebayang@users.noreply.github.com>
2021-07-24 10:26:41 +0700

    Create restart.sh

---
+++ restart.sh
@@ -0,0 +1,10 @@
+  
+# Restart the network service
+# Once all the configurations are successfully applied, restart the Network-Manager service by running the following command:
+```
+sudo systemctl restart network-manager
+```
+# If you are using a Ubuntu Server, instead use the following command:
+```
+sudo systemctl restart system-networkd
+```
